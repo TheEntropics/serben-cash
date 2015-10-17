38081fe76431d5ab2be3ea965480d3069f3256a1
Edoardo Morassutto <gianluigi988@gmail.com>
2015-10-17 22:20:09 +0200

    Formatted dates and checkboxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def true_mark
+    check_box_tag nil, 1, true, disabled: true
+  end
+  def false_mark
+    check_box_tag nil, 0, false, disabled: true
+  end
+
+  def format_date(date)
+    date.strftime("%d/%m/%Y")
+  end
 end
