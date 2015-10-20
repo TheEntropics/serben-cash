f8b756b52e3a60896183ad0e2abfc821e9fdc77b
Edoardo Morassutto <gianluigi988@gmail.com>
2015-10-20 19:59:17 +0200

    Added the ticks instead of true/false

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,10 @@ module ApplicationHelper
     check_box_tag nil, 0, false, disabled: true
   end
 
+  def boolean_mark(status)
+    status ? true_mark : false_mark
+  end
+
   def format_date(date)
     date.strftime("%d/%m/%Y")
   end
