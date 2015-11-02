0a6a3651175d8410ff5a7a43a99e0b420c49f912
Edoardo Morassutto <gianluigi988@gmail.com>
2015-11-02 18:00:31 +0100

    Sorded users in the add payment form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :username
 
+  default_scope ->{ order('deleted ASC, banned ASC, name') }
+
   has_many :roles, through: :users_role
   has_many :users_role
   has_many :payments
